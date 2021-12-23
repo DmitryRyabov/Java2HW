d82a2460e375da3268af63807ea7bcea8360026c
Dmitry <ryabov.d.s@yandex.ru>
2021-12-23 17:04:46 +0500

    Погода

---
+++ Main.java
@@ -0,0 +1,10 @@
+import com.squareup.okhttp.Request;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(Temperatyre.sendTempRequest("292332"));
+    }
+}
